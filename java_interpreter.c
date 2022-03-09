922104ddfa567e4e611697a36ca86874c7058131
Mehedee Hassan <Mehedee-Hassan@users.noreply.github.com>
2022-03-09 17:29:13 +0900

    todo list

--- java_interpreter.c
+++ java_interpreter.c
@@ -23,13 +23,25 @@ int main (void) {
         if (cnt == 0 | cnt == 10){
             printf("Press CTRL+C or CTRL+z to exit.\n");
             // show this message after 9 steps 
-
+            
             cnt = 1;
         }
 
 
         characters = getline(&buffer,&buffersize,stdin);
+                       
+        """
+        TODO:
+        1. find a way to write single line with edit capability 
+            a. now if we press arrow key ,console catch the chracter and prints
+            b. cannot edit properly
+            c. SOLUTION : catch all the chracters and only print the alpha neumeric characters
+            d. TODO: find a way to navigate
 
+        2. create class file and add a method
+        3. append current lines code to the code
+        4. if enter pressed run and show the result
+        """
         // getchar(); // hold the screen 
 
 
